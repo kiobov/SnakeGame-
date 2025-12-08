5eddf464922df8fc87bcb916ab4c3794ced5b03d
nyakio <nyakio19roseary@gmail.com>
2025-12-08 22:40:50 +0300

    Added functions, return functions in main, drawn the outside wall

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,50 +1,37 @@
 
-
 #include <iostream>
 using namespace std;
 bool gameOver;
 const int width = 20;
-const int  height = 20;
-int x, y, fruitX, fruitY, score;
-enum eDirection {STOP=0, LEFT,RIGHT, UP,DOWN};
-eDirection dir;
+const int height=20;
 
 
 void Setup() {
-    gameOver = false;
-    dir = STOP;
-    x = width / 2;
-    y = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
-    score = 0; 
-
+	gameOver = false;
 }
-
 void Draw() {
-    system("cls");
-    for (int i = 0; i < width+2; i++)
-        cout << "#";
-    cout<< endl;
+	system("cls");
+	for (int i = 0; i < width+2; i++) 
+		cout << "#";
+	cout << endl;
+	
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width;j++) {
-            if (j == 0)
-                cout << "#";
-            
-            
-                cout << " ";
-            if (j == width - 1)
-                cout << "#";
-            
-        }
-        cout<< endl;
-    }
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++) {
+			if (j == 0)
+				cout << "#";
+			cout << " ";
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
 
-
-    for (int i = 0; i < width+2; i++)
-        cout << "#";
-        cout<<endl; 
+	for (int i = 0; i < width+2; i++) 
+		cout << "#";
+	cout << endl;
+	
 
 }
 void Input() {
@@ -54,19 +41,13 @@ void Logic() {
 
 }
 
+int main() {
+	Setup();
+	while (!gameOver) {
+		Draw();
+		Input();
+		Logic();
 
-
-
-int main()
-{
-    Setup();
-    while (!gameOver) {
-        Draw();
-        Input();
-        Logic();
-
-
-    }
-    return 0;
+	}
+	return 0;
 }
-
