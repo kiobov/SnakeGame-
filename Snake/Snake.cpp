a7b94769c51c55009416638ad032224c833a52af
nyakio <nyakio19roseary@gmail.com>
2025-12-11 18:07:58 +0300

    Draw game walls and added setup and graw in main

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -24,15 +24,15 @@ void Draw() {
 	for (int i = 0; i < width+2; i++) 
 		cout << "#";
 	cout << endl;
-	
 
 	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < width; j++) {
-			if (j == 0)
+		for (int j = 0; j < width; j++)
+		{
+			if ( j == 0)
 				cout << "#";
 			cout << " ";
-			if (j == width - 1)
+			if ( j ==width - 1)
 				cout << "#";
 		}
 		cout << endl;
@@ -53,11 +53,14 @@ void Logic() {
 
 int main() {
 	Setup();
-	while (!gameOver) {
+	while (!Gameover) {
+		
 		Draw();
-		Input();
-		Logic();
 
 	}
+	
+
 	return 0;
+
+
 }
