fa14b1139a5055457d8ed2f41adcf99e69c56170
nyakio <nyakio19roseary@gmail.com>
2025-12-11 18:07:17 +0300

    Game setup

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,14 +1,24 @@
 
 #include <iostream>
 using namespace std;
-bool gameOver;
 const int width = 20;
-const int height=20;
+const int  height = 20;
+int x, y,  fruitX, fruitY, score ;
+enum eDirection {STOP=0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+bool Gameover ;
 
 
 void Setup() {
-	gameOver = false;
+	Gameover = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitX = rand()% width;
+	fruitY = rand() % height;
+
 }
+
 void Draw() {
 	system("cls");
 	for (int i = 0; i < width+2; i++) 
